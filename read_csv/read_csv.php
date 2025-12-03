1dc7779643d97523acdc8a19724a4eec8aa4ce35
Kavithma-Thushal <kavithmathushal9007@gmail.com>
2025-12-03 11:52:22 +0530

    read_csv.php modded

--- read_csv/read_csv.php
+++ read_csv/read_csv.php
@@ -1,6 +1,5 @@
 <?php
 
-// Load CSV
 $csv = '../student_marks.csv';
 
 // Read CSV
@@ -14,17 +13,17 @@ while (($row = fgetcsv($file)) !== false) {
 // Close CSV
 fclose($file);
 
+echo "<h1>Student Marks</h1>";
+
 // Display data
 echo "<table border='1' cellpadding='5'>";
 
-// Display header
 echo "<tr>";
 foreach ($data[0] as $header) {
     echo "<th>$header</th>";
 }
 echo "</tr>";
 
-// Display data rows
 for ($i = 1; $i < count($data); $i++) {
     echo "<tr>";
     foreach ($data[$i] as $cell) {
